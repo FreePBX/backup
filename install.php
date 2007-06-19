97661db4ca92bf5ae6f2604647004b4ce8d483e2
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2007-06-19 01:53:56 +0000

    #1992: move create/remove schema for Backup into module away from core

---
+++ install.php
@@ -0,0 +1,32 @@
+<?php
+
+global $db;
+global $amp_conf;
+
+$autoincrement = (($amp_conf["AMPDBENGINE"] == "sqlite") || ($amp_conf["AMPDBENGINE"] == "sqlite3")) ? "AUTOINCREMENT":"AUTO_INCREMENT";
+
+$sql = "
+CREATE TABLE Backup (
+	  Name varchar(50) default NULL,
+	  Voicemail varchar(50) default NULL,
+	  Recordings varchar(50) default NULL,
+	  Configurations varchar(50) default NULL,
+	  CDR varchar(55) default NULL,
+	  FOP varchar(50) default NULL,
+	  Minutes varchar(50) default NULL,
+	  Hours varchar(50) default NULL,
+	  Days varchar(50) default NULL,
+	  Months varchar(50) default NULL,
+	  Weekdays varchar(50) default NULL,
+	  Command varchar(200) default NULL,
+	  Method varchar(50) default NULL,
+	  ID int(11) NOT NULL $autoincrement,
+	  PRIMARY KEY  (ID)
+);";
+
+$check = $db->query($sql);
+if(DB::IsError($check)) {
+	die("Can not create Backup table");
+}
+
+?>
---
+++ uninstall.php
@@ -0,0 +1,16 @@
+<?php
+
+global $db;
+global $asterisk_conf;
+$sql = "DELETE FROM Backup";
+	$result = $db->query($sql);
+	if(DB::IsError($result)) {
+		die($result->getMessage());
+	}
+	$Cron_Script=$asterisk_conf['astvarlibdir']."/bin/retrieve_backup_cron_from_mysql.pl";
+	exec($Cron_Script);
+}
+
+sql('DROP TABLE Backup');
+
+?>

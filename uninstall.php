b72fe8354dc0150ee8a61de62ab4f93bb2808054
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2007-08-08 01:49:23 +0000

    found syntax error hiding in uninstall.php script of backup

--- uninstall.php
+++ uninstall.php
@@ -3,13 +3,12 @@
 global $db;
 global $asterisk_conf;
 $sql = "DELETE FROM Backup";
-	$result = $db->query($sql);
-	if(DB::IsError($result)) {
-		die($result->getMessage());
-	}
-	$Cron_Script=$asterisk_conf['astvarlibdir']."/bin/retrieve_backup_cron_from_mysql.pl";
-	exec($Cron_Script);
+$result = $db->query($sql);
+if(DB::IsError($result)) {
+	die($result->getMessage());
 }
+$Cron_Script=$asterisk_conf['astvarlibdir']."/bin/retrieve_backup_cron_from_mysql.pl";
+exec($Cron_Script);
 
 sql('DROP TABLE Backup');
 

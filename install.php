48c4555f48f5c88702a440b8d90d564c4768a31c
Ethan Schroeder <ethan.schroeder@gmail.com>
2008-08-03 05:59:08 +0000

    Sqlite3-related fixes that have grown since 2.3. Closes #2987

--- install.php
+++ install.php
@@ -4,26 +4,47 @@ global $db;
 global $amp_conf;
 
 $autoincrement = (($amp_conf["AMPDBENGINE"] == "sqlite") || ($amp_conf["AMPDBENGINE"] == "sqlite3")) ? "AUTOINCREMENT":"AUTO_INCREMENT";
-
-$sql = "
-CREATE TABLE IF NOT EXISTS Backup (
-	  Name varchar(50) default NULL,
-	  Voicemail varchar(50) default NULL,
-	  Recordings varchar(50) default NULL,
-	  Configurations varchar(50) default NULL,
-	  CDR varchar(55) default NULL,
-	  FOP varchar(50) default NULL,
-	  Minutes varchar(50) default NULL,
-	  Hours varchar(50) default NULL,
-	  Days varchar(50) default NULL,
-	  Months varchar(50) default NULL,
-	  Weekdays varchar(50) default NULL,
-	  Command varchar(200) default NULL,
-	  Method varchar(50) default NULL,
-	  ID int(11) NOT NULL $autoincrement,
-	  PRIMARY KEY  (ID)
-);";
-
+if($amp_conf["AMPDBENGINE"] == "sqlite3")  {
+	$sql = "
+	CREATE TABLE IF NOT EXISTS Backup (
+		  Name varchar(50) default NULL,
+		  Voicemail varchar(50) default NULL,
+		  Recordings varchar(50) default NULL,
+		  Configurations varchar(50) default NULL,
+		  CDR varchar(55) default NULL,
+		  FOP varchar(50) default NULL,
+		  Minutes varchar(50) default NULL,
+		  Hours varchar(50) default NULL,
+		  Days varchar(50) default NULL,
+		  Months varchar(50) default NULL,
+		  Weekdays varchar(50) default NULL,
+		  Command varchar(200) default NULL,
+		  Method varchar(50) default NULL,
+		  ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+	);
+	";
+}
+else  {
+	$sql = "
+	CREATE TABLE IF NOT EXISTS Backup (
+		  Name varchar(50) default NULL,
+		  Voicemail varchar(50) default NULL,
+		  Recordings varchar(50) default NULL,
+		  Configurations varchar(50) default NULL,
+		  CDR varchar(55) default NULL,
+		  FOP varchar(50) default NULL,
+		  Minutes varchar(50) default NULL,
+		  Hours varchar(50) default NULL,
+		  Days varchar(50) default NULL,
+		  Months varchar(50) default NULL,
+		  Weekdays varchar(50) default NULL,
+		  Command varchar(200) default NULL,
+		  Method varchar(50) default NULL,
+		  ID int(11) NOT NULL AUTO_INCREMENT,
+		  PRIMARY KEY  (ID)
+	);
+	";
+}
 $check = $db->query($sql);
 if(DB::IsError($check)) {
 	die_freepbx("Can not create Backup table");

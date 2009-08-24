0cefa1698f39cf7a9a85c9abdb7b8f8144a4ab95
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2009-08-24 00:30:48 +0000

    closes #3577 pull amportal.conf location from ENV

--- bin/dumpastdb.php
+++ bin/dumpastdb.php
@@ -29,11 +29,19 @@ function getconf($filename) {
         return $conf;
 }
 
-$amp_conf = getconf("/etc/amportal.conf");
+$amportalconf = (isset($_ENV["FREEPBXCONFIG"]) && strlen($_ENV["FREEPBXCONFIG"])) ? $_ENV["FREEPBXCONFIG"] : "/etc/amportal.conf";
+$amp_conf = getconf($amportalconf);
+
+if (isset($amp_conf["ASTMANAGERHOST"])) {
+  $amp_conf["ASTMANAGERHOST"] = '127.0.0.1';
+}
+if (isset($amp_conf["ASTMANAGERPORT"])) {
+  $amp_conf["ASTMANAGERPORT"] = '5038';
+}
 
 require_once($amp_conf['AMPWEBROOT']."/admin/common/php-asmanager.php");
 $astman         = new AGI_AsteriskManager();
-if (! $res = $astman->connect("127.0.0.1", $amp_conf["AMPMGRUSER"] , $amp_conf["AMPMGRPASS"])) {
+if (! $res = $astman->connect($amp_conf["ASTMANAGERHOST"] . ":".$amp_conf["ASTMANAGERPORT"], $amp_conf["AMPMGRUSER"] , $amp_conf["AMPMGRPASS"])) {
         unset( $astman );
 }
 

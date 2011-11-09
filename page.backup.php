fabdc4246f7c12a3688d2dc1583b71705a1f6b61
Moshe Brevda <mbrevda@gmail.com>
2011-11-09 18:27:55 +0000

    closes #5443 - paging improvements, revert some accidental checkins. Bugs/more features in NEW tickets please

--- page.backup.php
+++ page.backup.php
@@ -61,7 +61,7 @@ switch ($var['action']) {
 			ob_end_clean();
 		}
 
-		ob_start();
+		ob_start($amp_conf['buffering_callback']);
 		header('Content-Type: text/event-stream');
 		header('Cache-Control: no-cache');
 		//header('Expires: ' . date('r', time() + 60));

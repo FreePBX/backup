b8db9a78cf729b56e209dd45d64f038cfd69a518
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-16 09:03:34 -0500

    Update for license file

--- install.php
+++ install.php
@@ -1,8 +1,6 @@
 <?php
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
-//	License for all code of this FreePBX module can be found in the license file inside the module directory
-//	Copyright 2013 Schmooze Com Inc.
-//
+
 global $db, $amp_conf;
 
 $autoincrement = ($amp_conf["AMPDBENGINE"] == "sqlite3") ? "AUTOINCREMENT" : "AUTO_INCREMENT";

d644d4b5e03912ba3f72b6f5cdea73879c71bd44
Bryan Walters <secretop@gmail.com>
2014-09-25 22:56:13 -0400

    FREEPBX-8045 - onclick won't bind to new elements being added to the DOM, binding to document will though

--- assets/js/views/templates.js
+++ assets/js/views/templates.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
 	//delete rows on click
-	$('.delete_entrie').on('click', function(){
+	$(document).on('click', '.delete_entrie', function(){
 		$(this).closest('tr').fadeOut('normal', function(){$(this).closest('tr').remove();})
 	});
-	
+
 	//move add row's select box to right after the add button - easier to show and hide that way
 	$('select[name=add_tr_select]').insertAfter($('#add_entry'));
-	
+
 	//show select when add is clicked
 	$('#add_entry').click(function(){
 		$(this).hide();
 		$('select[name=add_tr_select]').val('').show();
 	});
-	
+
 	//hide select and add a new row when add_tr_select is selected
 	$('select[name=add_tr_select]').change(function(){
 		$(this).hide();
 		$('#add_entry').show();
 		add_template_row($(this).val());
 	});
-	
+
 	$('#template_table').closest('form').submit(function(){
 		var table = [];
 		$('#template_table > tbody > tr:not(:first)').each(function(){
@@ -32,7 +32,7 @@ $(document).ready(function(){
 			} else {
 				row.path = '';
 			}
-			
+
 			//ensure all "excludes" are unique
 			row.exclude	= $(this).find('td').eq(2).find('textarea').val()
 								.split("\n")
@@ -45,34 +45,34 @@ $(document).ready(function(){
 								})
 								.join("\n");
 			$(this).find('td').eq(2).find('textarea').val(row.exclude);
-					
+
 			row.index = $(this).index();
 			table.push(row);
 		});
-		
+
 		//loop through the array twice, ignore the same index (they will always match!)
 		//if there is a duplicate, sound the alarm
 		for (var row in table) {
 			for (var hash in table) {
 				if (table[row].index != table[hash].index
-					&& table[row].type == table[hash].type 
+					&& table[row].type == table[hash].type
 					&& table[row].path == table[hash].path
 				) {
 					alert('Duplicate items detected. Lines: ' + table[row].index + ', ' + table[hash].index);
-					
+
 					//scroll to the heighest problimatic line
 					line = table[row].index < table[hash].index ? table[row].index : table[hash].index;
 					var scrollto = $('#template_table > tbody > tr').eq(line).offset().top;
 					$('body,html').animate({scrollTop:scrollto}, 500);
-					
+
 					//dont submit the page
 					return false;
 				}
 			}
 		}
-		
+
 	});
-	
+
 })
 
 function add_template_row(type) {

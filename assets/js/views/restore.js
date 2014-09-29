b738cf1971b78bc20c1e5b5cb92b4ad3d36ce56d
Rob Thomas <xrobau@gmail.com>
2014-09-30 06:30:08 +1000

    Security warnings were breaking the JSON.

--- assets/js/views/restore.js
+++ assets/js/views/restore.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
 				'url': window.href,
 				'data': function (n) { 
 					return { 
+						'quietmode': true,
 						'action': 'list_dir', 
 						'path': $(n).data('path')
 					}; 

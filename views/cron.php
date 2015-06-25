e5608549f2faed9551ef78541d3d1853f3269129
Rob Thomas <xrobau@gmail.com>
2015-06-25 16:04:52 +1000

    Remove gettext error

--- views/cron.php
+++ views/cron.php
@@ -118,7 +118,6 @@ for($i = 1; $i < 32; $i++) {
 				<div class="form-group">
 					<div class="col-md-3">
 						<label class="control-label" for="cron_schedule"><?php echo _("Run Automatically") ?></label>
-						<i class="fa fa-question-circle fpbx-help-icon" data-for="cron_schedule"></i>
 					</div>
 					<div class="col-md-9">
 						<select class="form-control" id="cron_schedule" name="cron_schedule">
@@ -127,11 +126,6 @@ for($i = 1; $i < 32; $i++) {
 					</div>
 				</div>
 			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-12">
-			<span id="cron_schedule-help" class="help-block fpbx-help-block"><?php echo _("")?></span>
 		</div>
 	</div>
 </div>

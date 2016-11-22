4af351088ca9358aea7e81d1083e78ec8aaffb15
Rob Thomas <xrobau@gmail.com>
2016-11-22 14:47:27 +1000

    Remove gettext reserved string

--- views/restore/restore.php
+++ views/restore/restore.php
@@ -17,7 +17,6 @@ $dataurl = "ajax.php?module=backup&command=getJSON&jdata=serverGrid";
               <div class="form-group">
                 <div class="col-md-3">
                   <label class="control-label" for="upload"><?php echo _("Upload File") ?></label>
-                  <i class="fa fa-question-circle fpbx-help-icon" data-for="upload"></i>
                 </div>
                 <div class="col-md-9">
                   <span class="btn btn-default btn-file">
@@ -27,11 +26,6 @@ $dataurl = "ajax.php?module=backup&command=getJSON&jdata=serverGrid";
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12">
-            <span id="upload-help" class="help-block fpbx-help-block"><?php echo _("")?></span>
           </div>
         </div>
       </div>

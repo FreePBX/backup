c46ae4e1dd47a5bcf0b7a03d77f888689fd69c55
James Finstrom <jfinstrom@sangoma.com>
2017-11-08 17:18:48 -0700

    FREEPBX-15325 validate template on submit

--- assets/js/views/templates.js
+++ assets/js/views/templates.js
@@ -19,62 +19,64 @@ $(document).ready(function(){
 		$('#add_entry').show();
 		add_template_row($(this).val());
 	});
+	function validate(){
+		$('#template_table').closest('form').submit(function(){
+			var table = [];
+			$('#template_table > tbody > tr:not(:first)').each(function(){
+				row = {};
+				row.type 	= $(this).find('td').eq(0).find('input').val();
+				if ($(this).find('td').eq(1).find('select').length > 0) {
+					row.path = $(this).find('td').eq(1).find('select').val();
+				} else if ($(this).find('td').eq(1).find('input').length > 0) {
+					row.path = $(this).find('td').eq(1).find('input').val();
+				} else {
+					row.path = '';
+				}
 
-	$('#template_table').closest('form').submit(function(){
-		var table = [];
-		$('#template_table > tbody > tr:not(:first)').each(function(){
-			row = {};
-			row.type 	= $(this).find('td').eq(0).find('input').val();
-			if ($(this).find('td').eq(1).find('select').length > 0) {
-				row.path = $(this).find('td').eq(1).find('select').val();
-			} else if ($(this).find('td').eq(1).find('input').length > 0) {
-				row.path = $(this).find('td').eq(1).find('input').val();
-			} else {
-				row.path = '';
-			}
+				//ensure all "excludes" are unique
+				var ta = $(this).find('td').eq(2).find('textarea').val();
+				if(typeof ta !== "undefined") {
+					row.exclude	= ta.split("\n")
+									.filter(function(element){return element})
+									.sort()
+									.filter(function(element, index, array){
+										if ($.trim(element) != $.trim(array[index + 1])) {
+											return $.trim(element);
+										}
+									})
+									.join("\n");
+					$(this).find('td').eq(2).find('textarea').val(row.exclude);
+				}
 
-			//ensure all "excludes" are unique
-			var ta = $(this).find('td').eq(2).find('textarea').val();
-			if(typeof ta !== "undefined") {
-				row.exclude	= ta.split("\n")
-								.filter(function(element){return element})
-								.sort()
-								.filter(function(element, index, array){
-									if ($.trim(element) != $.trim(array[index + 1])) {
-										return $.trim(element);
-									}
-								})
-								.join("\n");
-				$(this).find('td').eq(2).find('textarea').val(row.exclude);
-			}
+				row.index = $(this).index();
+				table.push(row);
+			});
 
-			row.index = $(this).index();
-			table.push(row);
-		});
+			//loop through the array twice, ignore the same index (they will always match!)
+			//if there is a duplicate, sound the alarm
+			for (var row in table) {
+				for (var hash in table) {
+					if (table[row].index != table[hash].index
+						&& table[row].type == table[hash].type
+						&& table[row].path == table[hash].path
+					) {
+						alert('Duplicate items detected. Lines: ' + table[row].index + ', ' + table[hash].index);
 
-		//loop through the array twice, ignore the same index (they will always match!)
-		//if there is a duplicate, sound the alarm
-		for (var row in table) {
-			for (var hash in table) {
-				if (table[row].index != table[hash].index
-					&& table[row].type == table[hash].type
-					&& table[row].path == table[hash].path
-				) {
-					alert('Duplicate items detected. Lines: ' + table[row].index + ', ' + table[hash].index);
+						//scroll to the heighest problimatic line
+						line = table[row].index < table[hash].index ? table[row].index : table[hash].index;
+						var scrollto = $('#template_table > tbody > tr').eq(line).offset().top;
+						$('body,html').animate({scrollTop:scrollto}, 500);
 
-					//scroll to the heighest problimatic line
-					line = table[row].index < table[hash].index ? table[row].index : table[hash].index;
-					var scrollto = $('#template_table > tbody > tr').eq(line).offset().top;
-					$('body,html').animate({scrollTop:scrollto}, 500);
-
-					//dont submit the page
-					return false;
+						//dont submit the page
+						return false;
+					}
 				}
 			}
-		}
 
-	});
-
+		});
+	}
+	validate();
+	$('#backup_template').submit(validate);
 })
 
 function add_template_row(type) {

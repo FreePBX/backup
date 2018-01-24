db47e1b95523c47c86915ec5fb9ecbf057913212
Franck Danard <fdanard@sangoma.com>
2018-01-24 16:28:17 +0100

    Remove duplicate row with  asterrisk db

--- assets/js/views/backup.js
+++ assets/js/views/backup.js
@@ -143,6 +143,29 @@ function current_items_over_helper(action) {
 			break;
 	}
 }
+
+function removeDuplicateRows($table){
+	
+	// Removing duplicate crazy row containing "asterisk db"
+    function getVisibleRowText($row){
+        return $row.find('td:visible').text().toLowerCase().trim();
+    }
+    
+    $table.find('tr').each(function(index, row){
+        var $row = $(row);
+    
+        $row.nextAll('tr').each(function(index, next){
+            var $next = $(next);
+			var truc = getVisibleRowText($next);
+			console.log(truc);
+            if((getVisibleRowText($next).trim == getVisibleRowText($row).trim) && getVisibleRowText($next) == "asterisk db")
+				
+				// We remove this duplicated line.
+                $next.remove();
+        })
+    });
+}
+
 function add_template(template) {
 
 
@@ -164,6 +187,11 @@ function add_template(template) {
 			}
 
 			row.exclude	= $(this).find('td').eq(2).find('textarea').val() || '';
+			
+			if(template[item].type == "astdb"){
+				removeDuplicateRows($('#template_table'));						// Removing duplicate crazy row containing "asterisk db"
+			}
+
 			if (row.type == template[item].type && row.path == template[item].path) {
 				//merge excludes if we have any
 				if (template[item].exclude) {

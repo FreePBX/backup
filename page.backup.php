f0b2590009527b4972bd1ffd7096f73cf2ba6bc2
James Finstrom <jfinstrom@sangoma.com>
2018-04-27 21:14:50 +0000

    Cleanup, merge conflict

--- page.backup.php
+++ page.backup.php
@@ -2,55 +2,56 @@
 $backupClass = FreePBX::Backup();
 ?>
 <div class="container-fluid">
-<div class="display full-border">
-	<div class="row">
-		<div class="col-sm-12">
-			<div class="fpbx-container">
-				<div class="display full-border">
-					<ul class="nav nav-tabs">
-						<li role="presentation" class='<?php echo ((isset($_GET[' view ']) && $_GET['view '] == 'form ') || !isset($_GET['view
-							']))?"active":""?>'>
-							<a href="?display=backup">
-								<?php echo _("Backup")?>
-							</a>
-						</li>
-						<li role="presentation">
-							<a href="?display=backup_restore">
-								<?php echo _("Restore")?>
-							</a>
-						</li>
-						<!--Hide for alpha/beta... -->
-						<?php echo $transfer?>
-						<li role="presentation" class='<?php echo ((isset($_GET[' view ']) && $_GET['view '] == 'settings '))?"active":""?>'>
-							<a href="?display=backup&view=settings">
-								<?php echo _("Global Backup Settings")?>
-							</a>
-						</li>
-					</ul>
-					<br/>
-					<?php echo $backupClass->showPage('backup'); ?>
+	<div class="display full-border">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="fpbx-container">
+					<div class="display full-border">
+						<ul class="nav nav-tabs">
+							<li role="presentation" class='<?php echo ((isset($_GET[' view ']) && $_GET['view '] == 'form ') || !isset($_GET['view
+								']))?"active":""?>'>
+								<a href="?display=backup">
+									<?php echo _("Backup")?>
+								</a>
+							</li>
+							<li role="presentation">
+								<a href="?display=backup_restore">
+									<?php echo _("Restore")?>
+								</a>
+							</li>
+							<!--Hide for alpha/beta... -->
+							<?php echo $transfer?>
+							<li role="presentation" class='<?php echo ((isset($_GET[' view ']) && $_GET['view '] == 'settings '))?"active":""?>'>
+								<a href="?display=backup&view=settings">
+									<?php echo _("Global Backup Settings")?>
+								</a>
+							</li>
+						</ul>
+						<br/>
+						<?php echo $backupClass->showPage('backup'); ?>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<div class="modal fade" id="backuplog" tabindex="-1" role="dialog" aria-labelledby="backuplog" aria-hidden="true">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title" id="backupLogTitle">
-						<?php echo _("Backup Output")?>
-					</h5>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<div class="modal-body" style="overflow-y: auto;">
-					<img id="loadingimg" src="/admin/modules/backup/assets/images/backup.svg">
-					<hr/>
-					<span id="logtext"></span>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+		<div class="modal fade" id="backuplog" tabindex="-1" role="dialog" aria-labelledby="backuplog" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="backupLogTitle">
+							<?php echo _("Backup Output")?>
+						</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body" style="overflow-y: auto;">
+						<img id="loadingimg" src="/admin/modules/backup/assets/images/backup.svg">
+						<hr/>
+						<span id="logtext"></span>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					</div>
 				</div>
 			</div>
 		</div>
--- views/backup/warmspare.php
+++ views/backup/warmspare.php
@@ -1,104 +1,117 @@
-<div class="section-title" data-for="backup-warmspare"><h3><i class="fa fa-minus"></i> <?php echo _("Warm Spare") ?></h3></div>
+<div class="section-title" data-for="backup-warmspare">
+  <h3>
+    <i class="fa fa-minus"></i>
+    <?php echo _("Warm Spare") ?>
+  </h3>
+</div>
 <div class="section" data-id="backup-warmspare">
   <!--Enable-->
   <div class="element-container">
     <div class="row">
       <div class="form-group">
         <div class="col-md-3">
-          <label class="control-label" for="warmspareenables"><?php echo _("Enable") ?></label>
+          <label class="control-label" for="warmspareenables">
+            <?php echo _("Enable") ?>
+          </label>
           <i class="fa fa-question-circle fpbx-help-icon" data-for="warmspareenables"></i>
         </div>
         <div class="col-md-9 radioset">
-              <input type="radio" name="warmspareenables" id="warmspareenablesyes" value="yes" <?php echo ($warmspareenables == "yes"?"CHECKED":"") ?>>
-          <label for="warmspareenablesyes"><?php echo _("Yes");?></label>
-            <input type="radio" name="warmspareenables" id="warmspareenablesno" <?php echo ($warmspareenables == "yes"?"":"CHECKED") ?>>
-          <label for="warmspareenablesno"><?php echo _("No");?></label>
+          <input type="radio" name="warmspareenables" id="warmspareenablesyes" value="yes" <?php echo ($warmspareenables=="yes" ?
+            "CHECKED": "") ?>>
+          <label for="warmspareenablesyes">
+            <?php echo _("Yes");?>
+          </label>
+          <input type="radio" name="warmspareenables" id="warmspareenablesno" <?php echo ($warmspareenables=="yes" ? "": "CHECKED")
+            ?>>
+          <label for="warmspareenablesno">
+            <?php echo _("No");?>
+          </label>
         </div>
       </div>
     </div>
     <div class="row">
       <div class="col-md-12">
-        <span id="warmspareenables-help" class="help-block fpbx-help-block"><?php echo _("Should the warm spare feature be enabled")?></span>
+        <span id="warmspareenables-help" class="help-block fpbx-help-block">
+          <?php echo _("Should the warm spare feature be enabled")?>
+        </span>
       </div>
     </div>
   </div>
   <!--END Enable-->
   <!--Server Type-->
-<<<<<<< HEAD
-<<<<<<< HEAD
-  <div class="element-container">
-=======
   <div class="element-container warmspare">
->>>>>>> development/15.0
-=======
-  <div class="element-container warmspare">
->>>>>>> development/15.0
     <div class="row">
       <div class="form-group">
         <div class="col-md-3">
-          <label class="control-label" for="warmspare_type"><?php echo _("Server Type") ?></label>
+          <label class="control-label" for="warmspare_type">
+            <?php echo _("Server Type") ?>
+          </label>
           <i class="fa fa-question-circle fpbx-help-icon" data-for="warmspare_type"></i>
         </div>
         <div class="col-md-9 radioset">
-          <input type="radio" name="warmspare_type" id="warmspare_typeprimary" value="primary" <?php echo ($warmspare_type == "primary"?"CHECKED":"") ?>>
-          <label for="warmspare_typeprimary"><?php echo _("Primary");?></label>
-          <input type="radio" name="warmspare_type" id="warmspare_typespare" value="spare" <?php echo ($warmspare_type == "spare"?"CHECKED":"") ?>>
-          <label for="warmspare_typespare"><?php echo _("Spare");?></label>
+          <input type="radio" name="warmspare_type" id="warmspare_typeprimary" value="primary" <?php echo ($warmspare_type=="primary"
+            ? "CHECKED": "") ?>>
+          <label for="warmspare_typeprimary">
+            <?php echo _("Primary");?>
+          </label>
+          <input type="radio" name="warmspare_type" id="warmspare_typespare" value="spare" <?php echo ($warmspare_type=="spare" ?
+            "CHECKED": "") ?>>
+          <label for="warmspare_typespare">
+            <?php echo _("Spare");?>
+          </label>
         </div>
       </div>
     </div>
     <div class="row">
       <div class="col-md-12">
-        <span id="warmspare_type-help" class="help-block fpbx-help-block"><?php echo _("Is this the server that will be sending data (Primary) or receiving data (Spare)")?></span>
+        <span id="warmspare_type-help" class="help-block fpbx-help-block">
+          <?php echo _("Is this the server that will be sending data (Primary) or receiving data (Spare)")?>
+        </span>
       </div>
     </div>
   </div>
   <!--END Server Type-->
   <!--Enable Remote Trunks-->
-<<<<<<< HEAD
-<<<<<<< HEAD
-  <div class="element-container">
-=======
   <div class="element-container warmspare">
->>>>>>> development/15.0
-=======
-  <div class="element-container warmspare">
->>>>>>> development/15.0
     <div class="row">
       <div class="form-group">
         <div class="col-md-3">
-          <label class="control-label" for="warmspare_remote"><?php echo _("Enable Remote Trunks") ?></label>
+          <label class="control-label" for="warmspare_remote">
+            <?php echo _("Enable Remote Trunks") ?>
+          </label>
           <i class="fa fa-question-circle fpbx-help-icon" data-for="warmspare_remote"></i>
         </div>
         <div class="col-md-9 radioset">
-          <input type="radio" name="warmspare_remote" id="warmspare_remoteyes" value="yes" <?php echo ($warmspare_remote == "yes"?"CHECKED":"") ?>>
-          <label for="warmspare_remoteyes"><?php echo _("Yes");?></label>
-          <input type="radio" name="warmspare_remote" id="warmspare_remoteno" <?php echo ($warmspare_remote == "yes"?"":"CHECKED") ?>>
-          <label for="warmspare_remoteno"><?php echo _("No");?></label>
+          <input type="radio" name="warmspare_remote" id="warmspare_remoteyes" value="yes" <?php echo ($warmspare_remote=="yes" ?
+            "CHECKED": "") ?>>
+          <label for="warmspare_remoteyes">
+            <?php echo _("Yes");?>
+          </label>
+          <input type="radio" name="warmspare_remote" id="warmspare_remoteno" <?php echo ($warmspare_remote=="yes" ? "": "CHECKED")
+            ?>>
+          <label for="warmspare_remoteno">
+            <?php echo _("No");?>
+          </label>
         </div>
       </div>
     </div>
     <div class="row">
       <div class="col-md-12">
-        <span id="warmspare_remote-help" class="help-block fpbx-help-block"><?php echo _("Should the remote trunks be enabled")?></span>
+        <span id="warmspare_remote-help" class="help-block fpbx-help-block">
+          <?php echo _("Should the remote trunks be enabled")?>
+        </span>
       </div>
     </div>
   </div>
   <!--END Enable Remote Trunks-->
   <!--Remote IP-->
-<<<<<<< HEAD
-<<<<<<< HEAD
-  <div class="element-container">
-=======
   <div class="element-container warmspare">
->>>>>>> development/15.0
-=======
-  <div class="element-container warmspare">
->>>>>>> development/15.0
     <div class="row">
       <div class="form-group">
         <div class="col-md-3">
-          <label class="control-label" for="warmspare_remote"><?php echo _("Remote IP") ?></label>
+          <label class="control-label" for="warmspare_remote">
+            <?php echo _("Remote IP") ?>
+          </label>
           <i class="fa fa-question-circle fpbx-help-icon" data-for="warmspare_remote"></i>
         </div>
         <div class="col-md-9">
@@ -108,31 +121,35 @@
     </div>
     <div class="row">
       <div class="col-md-12">
-        <span id="warmspare_remote-help" class="help-block fpbx-help-block"><?php echo _("The address of the remote server.")?></span>
+        <span id="warmspare_remote-help" class="help-block fpbx-help-block">
+          <?php echo _("The address of the remote server.")?>
+        </span>
       </div>
     </div>
   </div>
   <!--END Remote IP-->
-<<<<<<< HEAD
-=======
   <!--Remote User-->
   <div class="element-container">
     <div class="row">
       <div class="form-group">
         <div class="col-md-3">
-          <label class="control-label" for="warmspare_user"><?php echo _("Remote User") ?></label>
-            <i class="fa fa-question-circle fpbx-help-icon" data-for="warmspare_user"></i>
-          </div>
-          <div class="col-md-9">
-            <input type="text" class="form-control" id="warmspare_user" name="warmspare_user" value="warmspare_user">
-          </div>
+          <label class="control-label" for="warmspare_user">
+            <?php echo _("Remote User") ?>
+          </label>
+          <i class="fa fa-question-circle fpbx-help-icon" data-for="warmspare_user"></i>
+        </div>
+        <div class="col-md-9">
+          <input type="text" class="form-control" id="warmspare_user" name="warmspare_user" value="warmspare_user">
         </div>
       </div>
-      <div class="row">
-        <div class="col-md-12">
-          <span id="warmspare_user-help" class="help-block fpbx-help-block"><?php echo _("User on remote system with proper permissions, usually root.")?></span>
-        </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <span id="warmspare_user-help" class="help-block fpbx-help-block">
+          <?php echo _("User on remote system with proper permissions, usually root.")?>
+        </span>
       </div>
+    </div>
   </div>
   <!--Remote User-->
   <!--Server Public Key-->
@@ -140,25 +157,31 @@
     <div class="row">
       <div class="form-group">
         <div class="col-md-3">
-          <label class="control-label" for="publickey"><?php echo _("Server Public Key") ?></label>
-            <i class="fa fa-question-circle fpbx-help-icon" data-for="publickey"></i>
-          </div>
-          <div class="col-md-9">
-            <textarea class="form-control" id="publickey" rows="8" name="publickey" <?php echo ($warmspare_type == 'primary')?'readonly':''?>><?php echo $publickey?></textarea>
-            <?php 
+          <label class="control-label" for="publickey">
+            <?php echo _("Server Public Key") ?>
+          </label>
+          <i class="fa fa-question-circle fpbx-help-icon" data-for="publickey"></i>
+        </div>
+        <div class="col-md-9">
+          <textarea class="form-control" id="publickey" rows="8" name="publickey" <?php echo ($warmspare_type=='primary' )?
+            'readonly': ''?>>
+            <?php echo $publickey?>
+          </textarea>
+          <?php 
               if(empty($publickey && $warmspare_type == 'primary')){
                 echo "<a href='?display=backup&view=newRSA' class='btn btn-default' id='generateRSA'>"._("Generate SSH keys")."</a>";
               }
             ?>
-          </div>
         </div>
       </div>
-      <div class="row">
-        <div class="col-md-12">
-          <span id="publickey-help" class="help-block fpbx-help-block"><?php echo _("This is the servers public key.")?></span>
-        </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <span id="publickey-help" class="help-block fpbx-help-block">
+          <?php echo _("This is the servers public key.")?>
+        </span>
       </div>
+    </div>
   </div>
   <!--Server Public Key-->
->>>>>>> development/15.0
 </div>

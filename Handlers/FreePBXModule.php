478cf3531c44a41c2605b7cf5820796d8218d663
James Finstrom <jfinstrom@sangoma.com>
2018-07-17 18:05:28 +0000

    Remove debug, fix file restore

--- Handlers/FreePBXModule.php
+++ Handlers/FreePBXModule.php
@@ -9,42 +9,42 @@ class FreePBXModule{
 		if ($freepbx == null) {
 			throw new \Exception('Not given a FreePBX Object');
 		}
-        $this->FreePBX = $freepbx;
-        $this->mf = \module_functions::create();
-    }
-    public function reset($module,$version){
-        $developer = $this->FreePBX->Config->get('DEVEL');
-        $module = \strtolower($module);
-        $xml = $this->mf->getModuleDownloadByModuleNameAndVersion($module, $version);
-        if(!empty($xml) && !$developer && $xml['version'] !== $version){
-            $this->processRemote($xml);
-        }
+		$this->FreePBX = $freepbx;
+		$this->mf = \module_functions::create();
+	}
+	public function reset($module,$version){
+		$developer = $this->FreePBX->Config->get('DEVEL');
+		$module = \strtolower($module);
+		$xml = $this->mf->getModuleDownloadByModuleNameAndVersion($module, $version);
+		if(!empty($xml) && !$developer && $xml['version'] !== $version){
+			$this->processRemote($xml);
+		}
 
-        $uninstall = $this->uninstall($module);
-        $install = $this->install($module);
-        return ($uninstall && $install);
-    }
-    public function processRemote($xml){
-        $module = $xml['rawname'];
-        $download =  $this->mf->handledownload($xml['downloadurl']);
-        if(is_array($download)){
-            return false;
-        }
-        return true;
-    }
-    public function install($module){
-        $install = $this->mf->install($module, 'true');
-        if(is_array($install)){
-            return false;
-        }
-        return true;
-    }
+		$uninstall = $this->uninstall($module);
+		$install = $this->install($module);
+		return ($uninstall && $install);
+	}
+	public function processRemote($xml){
+		$module = $xml['rawname'];
+		$download =  $this->mf->handledownload($xml['downloadurl']);
+		if(is_array($download)){
+			return false;
+		}
+		return true;
+	}
+	public function install($module){
+		$install = $this->mf->install($module, 'true');
+		if(is_array($install)){
+			return false;
+		}
+		return true;
+	}
 
-    public function uninstall($module){
-        $uninstall = $this->mf->uninstall($module, 'true');
-        if(is_array($uninstall)){
-            return false;
-        }
-        return true;
-    }
+	public function uninstall($module){
+		$uninstall = $this->mf->uninstall($module, 'true');
+		if(is_array($uninstall)){
+			return false;
+		}
+		return true;
+	}
 }
--- Handlers/SingleRestore.php
+++ Handlers/SingleRestore.php
@@ -11,59 +11,59 @@ use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
 class SingleRestore{
-    public function __construct($file, $freepbx){
-        $this->restoreFile = $file;
-        $this->FreePBX = $freepbx;
-        $this->Backup = $freepbx->Backup;
-        $this->out = new ConsoleOutput();
-        $this->progressBar = new ProgressBar($this->out, 4);
-        $this->progressBar->setFormatDefinition('custom', ' %current%/%max% -- %message%'.PHP_EOL);
-        $this->progressBar->setFormat('custom');
+	public function __construct($file, $freepbx){
+		$this->restoreFile = $file;
+		$this->FreePBX = $freepbx;
+		$this->Backup = $freepbx->Backup;
+		$this->out = new ConsoleOutput();
+		$this->progressBar = new ProgressBar($this->out, 4);
+		$this->progressBar->setFormatDefinition('custom', ' %current%/%max% -- %message%'.PHP_EOL);
+		$this->progressBar->setFormat('custom');
 
-        define('BACKUPTMPDIR', '/var/spool/asterisk/tmp');
-        define('REDTEXT', "\033[31m ");
-        define('GREENTEXT', "\033[32m ");
-        define('WHITETEXT', "\033[0m ");
-    }
+		define('BACKUPTMPDIR', '/var/spool/asterisk/tmp');
+		define('REDTEXT', "\033[31m ");
+		define('GREENTEXT', "\033[32m ");
+		define('WHITETEXT', "\033[0m ");
+	}
 
-    public function doSingleRestore(){
-        $phar = new PharData($this->restoreFile);
-        $restoreData = $phar->getMetadata();
-        $this->Backup->fs->remove(BACKUPTMPDIR);
-        $this->Backup->fs->mkdir(BACKUPTMPDIR);
-        $phar->extractTo(BACKUPTMPDIR);
-        $moduleInfo = $restoreData['module'];
-        foreach ($moduleInfo as $key => $value) {
-            if($key === 'builtin'){
-                continue;
-            }
-            $moduleName = $key;
-            $moduleInfo = $value;
-            break;
-        }
+	public function doSingleRestore(){
+		$phar = new PharData($this->restoreFile);
+		$restoreData = $phar->getMetadata();
+		$this->Backup->fs->remove(BACKUPTMPDIR);
+		$this->Backup->fs->mkdir(BACKUPTMPDIR);
+		$phar->extractTo(BACKUPTMPDIR);
+		$moduleInfo = $restoreData['module'];
+		foreach ($moduleInfo as $key => $value) {
+			if($key === 'builtin'){
+				continue;
+			}
+			$moduleName = $key;
+			$moduleInfo = $value;
+			break;
+		}
 
-        $modJson = BACKUPTMPDIR . '/modulejson/' . ucfirst($moduleName) . '.json';
-        if(!file_exists($modJson)){
-            exit(sprintf(REDTEXT ._("Can't find the module data for %s").PHP_EOL.WHITETEXT,$moduleName));
-        }
-        $modData = json_decode(file_get_contents($modJson), true);
-        $restore = new Model\Restore($this->Backup->FreePBX, $modData);    
-        echo REDTEXT."***"._("In single restore mode dependencies are NOT processed")."***".PHP_EOL.WHITETEXT;
-        $modulehandler = new FreePBXModule($this->FreePBX);
-        \modgettext::push_textdomain($moduleName);
-        $this->progressBar->setMessage(GREENTEXT._('Resetting module data, depending on the module it may take a bit.').WHITETEXT);
-        $this->progressBar->start();
-        $modulehandler->reset($moduleName, $moduleInfo['version']);
-        $this->progressBar->advance();
-        $class = sprintf('\\FreePBX\\modules\\%s\\Restore', ucfirst($moduleName));
-        $class = new $class($restore, $this->FreePBX, BACKUPTMPDIR);
-        $this->progressBar->advance();
-        $this->progressBar->setMessage(GREENTEXT . _('Restoring Data...') . WHITETEXT);
-        $class->runRestore('');
-        $this->progressBar->advance();
-        $this->progressBar->setMessage(GREENTEXT . _('Finished...') . WHITETEXT);
-        $this->progressBar->finish();
-        needreload();
-        $this->out->writeln("Please run fwconsole reload");
-    }
+		$modJson = BACKUPTMPDIR . '/modulejson/' . ucfirst($moduleName) . '.json';
+		if(!file_exists($modJson)){
+			exit(sprintf(REDTEXT ._("Can't find the module data for %s").PHP_EOL.WHITETEXT,$moduleName));
+		}
+		$modData = json_decode(file_get_contents($modJson), true);
+		$restore = new Model\Restore($this->Backup->FreePBX, $modData);    
+		echo REDTEXT."***"._("In single restore mode dependencies are NOT processed")."***".PHP_EOL.WHITETEXT;
+		$modulehandler = new FreePBXModule($this->FreePBX);
+		\modgettext::push_textdomain($moduleName);
+		$this->progressBar->setMessage(GREENTEXT._('Resetting module data, depending on the module it may take a bit.').WHITETEXT);
+		$this->progressBar->start();
+		$modulehandler->reset($moduleName, $moduleInfo['version']);
+		$this->progressBar->advance();
+		$class = sprintf('\\FreePBX\\modules\\%s\\Restore', ucfirst($moduleName));
+		$class = new $class($restore, $this->FreePBX, BACKUPTMPDIR);
+		$this->progressBar->advance();
+		$this->progressBar->setMessage(GREENTEXT . _('Restoring Data...') . WHITETEXT);
+		$class->runRestore('');
+		$this->progressBar->advance();
+		$this->progressBar->setMessage(GREENTEXT . _('Finished...') . WHITETEXT);
+		$this->progressBar->finish();
+		needreload();
+		$this->out->writeln("Please run fwconsole reload");
+	}
 }

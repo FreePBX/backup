5e8d1d4da6f3e10227cd528c1744d49307e691bf
James Finstrom <jfinstrom@sangoma.com>
2018-07-18 18:58:58 +0000

    Fix module reset bottleneck by trying local first.

--- Handlers/FreePBXModule.php
+++ Handlers/FreePBXModule.php
@@ -5,24 +5,24 @@
 namespace FreePBX\modules\Backup\Handlers;
 
 class FreePBXModule{
+    public $moduleXML = false;
 	public function __construct($freepbx = null) {
 		if ($freepbx == null) {
 			throw new \Exception('Not given a FreePBX Object');
 		}
 		$this->FreePBX = $freepbx;
-		$this->mf = \module_functions::create();
+        $this->mf = \module_functions::create();
 	}
 	public function reset($module,$version){
 		$developer = $this->FreePBX->Config->get('DEVEL');
-		$module = \strtolower($module);
-		$xml = $this->mf->getModuleDownloadByModuleNameAndVersion($module, $version);
-		if(!empty($xml) && !$developer && $xml['version'] !== $version){
+        $module = \strtolower($module);
+		if($this->getModuleVersion($module) !== $version && !$developer){
+            $xml = $this->mf->getModuleDownloadByModuleNameAndVersion($module, $version);
 			$this->processRemote($xml);
-		}
-
-		$uninstall = $this->uninstall($module);
-		$install = $this->install($module);
-		return ($uninstall && $install);
+        }
+        $uninstall = $this->uninstall($module);
+        $install = $this->install($module);
+		return $this;
 	}
 	public function processRemote($xml){
 		$module = $xml['rawname'];
@@ -46,5 +46,73 @@ class FreePBXModule{
 			return false;
 		}
 		return true;
-	}
+    }
+
+    //future functionality for resetting database and astdb
+
+    public function truncateTables($module){
+        $tables = $this->getTables($module);
+        $stmt = $this->FreePBX->Database->prepare('TRUNCATE TABLE :table');
+        foreach($tables as $table){
+            $stmt->execute([':table' => $table]);
+        }
+        return $this;
+    }
+    //future functionality clean data
+    public function cleanAstdb($module){
+        if(!$this->FreePBX->astman->connected()){
+            return false;
+        }
+        $keys = $this->getAstdb($module);
+        foreach ($keys as $key) {
+            $this->FreePBX->astman->database_deltree($key);
+        }
+        return $this;
+    }
+    
+    public function getTables($module){
+        $tables = [];
+        $this->loadModuleXML($module);
+        if (!$this->moduleXML) {
+            return [];
+        }
+        foreach ($this->moduleXML->database->table as $table) {
+            $tname = (string)$table->attributes()->name;
+            $tables[] = $tname;
+        }
+        return $tables;
+    }
+    
+    public function getAstdb($module){
+        $keys = [];
+        $this->loadModuleXML($module);
+        if(!$this->moduleXML){
+            return [];
+        }
+        foreach ($this->moduleXML->astdb->key as $key) {
+            $kname = (string)$table->attributes()->name;
+            $keys[] = $kname;
+        }
+        return $keys;
+    }
+    public function loadModuleXML($module){
+        if($this->ModuleXML){
+            return $this;
+        }
+        $dir = $this->FreePBX->Config->get('AMPWEBROOT') . '/admin/modules/' . $module;
+        if(!file_exists($dir.'/module.xml')){
+            $this->moduleXML = false;
+            return $this;
+        }
+        $xml = simplexml_load_file($dir . '/module.xml');
+        $this->moduleXML = $xml;
+        return $this;
+    }
+    public function getModuleVersion($module){
+        $this->loadModuleXML($module);
+        if(!$this->moduleXML){
+            return '';
+        }
+        return (string)$this->moduleXML->attributes()->version;
+    }
 }

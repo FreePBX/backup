9f94f872b196c1b4364faaac8b10580de6f7135e
James Finstrom <jfinstrom@sangoma.com>
2018-09-24 19:33:59 +0000

    FREEPBX-18210 Remove unimplemented tab

--- page.backup_restore.php
+++ page.backup_restore.php
@@ -12,7 +12,11 @@ $backupClass = FreePBX::Backup();
 					<ul class="nav nav-tabs">
 						<li role="presentation"><a href="?display=backup"><?php echo _("Backup")?></a></li>
 						<li role="presentation" class='active'><a href="?display=backup_restore"><?php echo _("Restore")?></a></li>
-						<li role="presentation"><a href="?display=backup&view=download"><?php echo _("Download")?></a></li>
+						<li role="presentation" class='<?php echo ((isset($_GET['view']) && $_GET['view'] == 'settings')) ? "active" : "" ?>'>
+							<a href="?display=backup&view=settings">
+								<?php echo _("Global Backup Settings") ?>
+							</a>
+						</li>
 					</ul>
 					<div class="display">
 						<?php echo $backupClass->showPage('restore'); ?>

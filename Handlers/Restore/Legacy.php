75a762f57d038d0ce68d1f609afc90bc55d28a26
Philip Joseph <jphilip@sangoma.com>
2019-12-04 10:29:35 +0000

    FREEI-951 cdr timeout
    	Setting time out to MAX 1 hour

--- Handlers/Restore/Legacy.php
+++ Handlers/Restore/Legacy.php
@@ -153,7 +153,8 @@ class Legacy extends Common {
 			$this->log(sprintf(_("Processing processLegacyCdr ' %s ' Command is Running now "), $command));
 			$process = new Process($command);
 			try {
-				$process->setTimeout(300); //set timeout to 5min
+				$this->log(_("This process may take long time based on the CDR SQL file size, setting time out to MAX 1 hour"));
+				$process->setTimeout(3600);
 				$process->mustRun();
 				$out = $process->getOutput();
 				$this->log(sprintf(_("Processing processLegacyCdr SQL Done....  %s  "), $out));

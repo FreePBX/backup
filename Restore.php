39958a8844e36d5e6d1b01a7adad70e818f1cea4
Philip Joseph <jphilip@sangoma.com>
2020-09-09 12:18:26 +0530

    FREEI-2062 Restore to 15 from a 13/14 backup not restoring POMPS backup settings
    	Restoring Legacy Filestore FTP from backup module

--- Restore.php
+++ Restore.php
@@ -7,6 +7,26 @@ class Restore Extends Base\RestoreBase{
 		$this->importKVStore($settings['kvstore']);
 	}
 	public function processLegacy($pdo, $data, $tables, $unknownTables){
-		$this->log('Skipping Legacy Backup module ');
+		$this->log('Restoring only Legacy Backup FTP Servers to Filestore');
+		$this->RestoreLegacyFtpFilestore($pdo);
+	}
+	
+	private function RestoreLegacyFtpFilestore($pdo){
+		//backup_server,backup_server_details
+		$bkserver = "SELECT id,name,desc FROM backup_servers WHERE type='ftp'";
+		$sth = $pdo->query($bkserver,\PDO::FETCH_ASSOC);
+		$serversar = $sth->fetchAll();
+		if(!empty($serversar)) {
+			foreach($serversar as $ser){
+				$server = ['id'=>'','action'=>'add','timeout'=>30,'name'=>$ser['name'],'desc'=>$ser['desc'],'driver'=>'FTP'];
+				$bkserverd = "SELECT key,value FROM backup_server_details WHERE server_id='".$ser['id']."'";
+				$sth = $pdo->query($bkserverd,\PDO::FETCH_ASSOC);
+				$res = $sth->fetchAll();
+				foreach($res as $row) {
+					$server[$row['key']] = $row['value'];
+				}
+				$this->FreePBX->Filestore->addItem('FTP',$server);
+			}
+		}
 	}
 }

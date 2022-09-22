5ac6880d6095556da164c885532e8498d56849c4
Megha BM <meghabm@sangoma.com>
2022-09-22 08:56:57 +0000

    New Bootstrap GUI fixesNew Bootstrap GUI fixes

--- views/runModal.php
+++ views/runModal.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="runModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document" style="width: 90%;">
+  <div class="modal-dialog modal-xl" role="document" style="width: 90%;">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>

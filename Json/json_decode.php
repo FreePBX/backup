3dc583664f0f8723e107186633b738ed8eeb6ffb
Akarsh MR <akarshmr@sangoma.com>
2025-08-28 10:47:12 +0000

    FREEI-2242 advr syncback to primary issue resolved

--- Json/json_decode.php
+++ Json/json_decode.php
@@ -4,7 +4,7 @@ namespace FreePBX\modules\Backup\Json;
 function json_decode($json, $assoc = false, $depth = 512, $options = 0) {
 	$data = \json_decode($json, $assoc, $depth, $options);
 	if (JSON_ERROR_NONE !== json_last_error()) {
-		throw new \JsonException(json_last_error_msg(),json_last_error());
+		return null;
 	}
 	return $data;
 }
